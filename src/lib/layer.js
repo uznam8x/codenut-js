ed4a71764d2cbe54737adcccd5d077457bb21885
jinam Yu <znu@thenet-global.com>
2018-03-22 23:08:49 +0900

    layer update

--- src/lib/layer.js
+++ src/lib/layer.js
@@ -2,16 +2,30 @@ define( ['./core'], (Codenut) => {
   'use strict';
   Codenut.layer = {
     item:[],
-    add: function (key) {
-      this.item.push(key);
-      document.querySelector('body').classList.add('open--layer');
+    add: function (el) {
+      const uuid = Codenut.util.uuid();
+      el.setAttribute('data-layer-id', uuid);
+      this.item.push(uuid);
+      document.querySelector('body').classList.add('layer--open');
+      if( !document.getElementById('layer__backdrop') ){
+        const backdrop = document.createElement('div');
+        backdrop.id = 'layer__backdrop';
+        document.getElementsByTagName('body')[0].appendChild(backdrop);
+        TweenMax.to(backdrop, .5, {opacity:.4, ease:Expo.easeOut});
+      }
     },
-    remove: function (key) {
+    remove: function (el) {
+      let key = el.getAttribute('data-layer-id') || 0;
       const index = this.item.indexOf(key);
       if( index > -1){
         this.item.splice(index, 1);
         if( !this.item.length ){
-          document.querySelector('body').classList.remove('open--layer');
+          const backdrop = document.getElementById('layer__backdrop');
+          TweenMax.killTweensOf(backdrop);
+          TweenMax.to(backdrop, .5, {opacity:0, ease:Expo.easeOut, onComplete:()=>{
+            document.querySelector('body').classList.remove('layer--open');
+            backdrop.parentNode.removeChild(backdrop);
+          }});
         }
       }
     },
--- webpack.config.js
+++ webpack.config.js
@@ -18,20 +18,5 @@ module.exports = {
         },
       },
     ],
-  },
-
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      }
-      ,
-      output: {
-        comments: false,
-      },
-      sourceMap: false,
-    }),
-  ],
-
-  watch:true
+  }
 };
