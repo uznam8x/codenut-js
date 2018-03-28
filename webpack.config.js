46df679e18dc7739fada7a2c595e73cd6c51f1cc
jinam Yu <znu@thenet-global.com>
2018-03-28 15:39:06 +0900

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,58 @@
 const webpack = require('webpack');
 const path = require('path');
 module.exports = {
-  entry: __dirname+'/src/codenut.js',
+  entry: __dirname + '/src/codenut.js',
   output: {
     path: __dirname + '/dist',
     filename: 'codenut.min.js',
   },
   cache: false,
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015'],
-        },
+        use:[
+          {
+            loader: 'babel-loader',
+            query: {
+              presets: ['es2015'],
+            },
+          }
+        ]
       },
       {
         test: /\.scss$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'style-loader!css-loader!sass-loader'
+        exclude: /node_module/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              outputStyle: 'compressed',
+              includePaths: [
+                path.resolve(__dirname, './node_modules/codenut-style/scss/')
+              ]
+            }
+          }
+        ]
       }
-    ],
-  }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+      output: {
+        comments: false,
+      },
+      sourceMap: false,
+    }),
+  ],
 };
