5b582c23b5183434a9df7c40cbd9933e0b8bea1b
jinam Yu <znu@thenet-global.com>
2018-03-30 16:29:56 +0900

    update

--- src/lib/core.js
+++ src/lib/core.js
@@ -1,8 +1,19 @@
 define( [], () => {
   'use strict';
   window.Codenut = window.Codenut || {};
-  window.Codenut.version = '1.0.12';
+  window.Codenut.version = '1.0.13';
   window.Codenut.website = 'http://codenut.prisf.com';
   window.Codenut.mail = 'uznam8x@gmail.com';
+
+  // Disable # link
+  document.addEventListener('click', (e) => {
+    let a = ( e.target.nodeName.toLowerCase() === 'a') ? e.target : e.target.closest('a');
+    if( a ){
+      const HREF = a.getAttribute('href');
+      if(HREF === '#'){
+        e.preventDefault();
+      }
+    }
+  });
   return window.Codenut;
 });
